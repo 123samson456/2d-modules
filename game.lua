2f2fe323be9084b5cca2cc63b90b86779a1ff86d
123samson456 <breitung.programming@gmx.de>
2016-07-04 11:22:11 +0200

    Create game.lua

---
+++ game.lua
@@ -0,0 +1,22 @@
+-- game.lua
+
+local class 	= 	require "lib.middleclass"
+local states	=	require "lib.stateful"
+local controls	= 	require "lib.controls"
+
+local Game	=	class( "Game" ):include( states )
+
+function Game:initialize( config )
+	self.config		= 	config
+	self.controls	=	controls	
+end
+
+function Game:update( dt )
+
+end
+
+function Game:draw()
+
+end
+
+return Game
