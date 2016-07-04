e6d1bca894e7f6695d044cbe830e11944319fa6c
123samson456 <breitung.programming@gmx.de>
2016-07-04 11:11:02 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
-# 2d-modules
+-- main.lua
+
+local Game	= 	require "game"
+local media	= 	require "media"
+
+function love.load()
+	game	= 	Game:new()
+	media.load()
+end
+
+function love.update( dt )
+	game:update()
+end
+
+function love.draw()
+	game:draw()
+end
