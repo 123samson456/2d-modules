868088a854309c2f14a72082450e11e0c792b9d2
123samson456 <breitung.programming@gmx.de>
2016-07-04 11:34:47 +0200

    Update controls.lua

--- lib/controls.lua
+++ lib/controls.lua
@@ -1,5 +1,11 @@
 -- controls.lua
 
-local c	=	 require "data.controls"
+local c		=	require "data.controls"
+local Input	=	require "lib.input"
+input	=	Input()
+
+for action, data in pairs( c ) do
+	input:bind( data.key, action )	
+end
 
 return c
