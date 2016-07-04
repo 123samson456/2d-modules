e0e3a4be5cb7da8517992093fde5e75bab0b9a7e
123samson456 <breitung.programming@gmx.de>
2016-07-04 11:32:06 +0200

    Create controls.lua

---
+++ data/controls.lua
@@ -0,0 +1,22 @@
+-- controls.lua
+
+local c	=	{}
+
+local c[ "game_up" ]	=	{
+	key		=	"w",
+	info 	=	""
+}
+local c[ "game_down" ]	=	{
+	key		=	"s",
+	info 	=	""
+}
+local c[ "game_left" ]	=	{
+	key		=	"a",
+	info 	=	""
+}
+local c[ "game_right" ]	=	{
+	key		=	"d",
+	info 	=	""
+}
+
+return c
