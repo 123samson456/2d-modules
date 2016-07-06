87f119b24058e7626aec28820e15cfd3d485e6e5
123samson456 <breitung.programming@gmx.de>
2016-07-06 13:03:02 +0200

    Update bullets.lua

--- bullets.lua
+++ bullets.lua
@@ -6,6 +6,7 @@ local Bullets	=	class( "Bullets" )
 
 function Bullets:initialize()
 	self.bullets	=	{}
+	self.drawDistance	=	30	-- in px
 end
 
 function Bullets:Add( x, y, angle, vel )
@@ -16,11 +17,23 @@ function Bullets:Remove( index )
 	table.remove( self.bullets, index )
 end
 
+function Bullets:Flush()
+	self.bullets	=	{}	
+end
+
 function Bullets:Iterate()
-	for i, bullet in ipairs( self.bullets ) do
+	local startx, starty = 0, 0
+	local screenx, screeny = love.graphics:getDimensions()
+	for index, bullet in ipairs( self.bullets ) do
 		-- Calculate new position of the bullet
 		
 		-- Check if the new position is outside the screen
+		if ( bullet.x < ( start.x - self.drawDistance ) ) or
+			( bullet.x > ( screen.x + self.drawDistance ) ) or
+			( bullet.y < ( start.y - self.drawDistance ) ) or
+			( bullet.y > ( screen.y + self.drawDistance ) ) then
+			self:Remove( index )	
+		end
 	end
 end
 
